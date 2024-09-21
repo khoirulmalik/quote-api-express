15bbe67d7de2c2b10da65deecb98ff79aca06f7d
malik <khoirulmalik03@gmail.com>
2024-09-21 22:43:24 +0700

    inisialisasi server

--- server.js
+++ server.js
@@ -7,3 +7,7 @@ const { getRandomElement } = require("./utils");
 const PORT = process.env.PORT || 4001;
 
 app.use(express.static("public"));
+
+app.listen(PORT, () => {
+  console.log(`Server running in http://localhost:${PORT}`);
+});
