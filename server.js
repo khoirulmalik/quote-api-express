c3431c30bd5e3a870644aa06de64eceee1bcbecd
malik <khoirulmalik03@gmail.com>
2024-09-21 23:25:52 +0700

    add API /random

--- server.js
+++ server.js
@@ -9,7 +9,13 @@ app.use("/api/quotes", apiRouter);
 
 const PORT = process.env.PORT || 4001;
 
+//middleware
 app.use(express.static("public"));
+
+apiRouter.get("/random", (req, res, next) => {
+  const randomQuote = getRandomElement(quotes);
+  res.send({ quote: randomQuote.quote });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running in http://localhost:${PORT}`);
