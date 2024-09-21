a0bdba04c89f737ac6676ea883de80512cfaad35
malik <khoirulmalik03@gmail.com>
2024-09-21 23:58:37 +0700

    add GET / and add getQuoteByPerson in utils

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 const { quotes } = require("./data");
-const { getRandomElement } = require("./utils");
+const { getRandomElement, getQuoteByPerson } = require("./utils");
 
 const apiRouter = express.Router();
 app.use("/api/quotes", apiRouter);
@@ -14,9 +14,26 @@ app.use(express.static("public"));
 
 apiRouter.get("/random", (req, res, next) => {
   const randomQuote = getRandomElement(quotes);
-  res.send({ quote: randomQuote.quote });
+  res.send({ quote: randomQuote });
 });
 
+apiRouter.get("/", (req, res, next) => {
+  const person = req.query.person;
+
+  if (person) {
+    const quotesByPerson = getQuoteByPerson(quotes, person);
+
+    if (quotesByPerson.length > 0) {
+      res.send({ quotes: quotesByPerson });
+    } else {
+      res
+        .status(404)
+        .send({ message: `Tidak ada kutipan yang ditemukan untuk ${person}` });
+    }
+  } else {
+    res.send({ quotes });
+  }
+});
 app.listen(PORT, () => {
   console.log(`Server running in http://localhost:${PORT}`);
 });
--- utils.js
+++ utils.js
@@ -1,8 +1,20 @@
-const getRandomElement = arr => {
-  if (!Array.isArray(arr)) throw new Error('Expected an array');
+const getRandomElement = (arr) => {
+  if (!Array.isArray(arr)) throw new Error("Expected an array");
   return arr[Math.floor(Math.random() * arr.length)];
-}
+};
 
-  module.exports = {
-  getRandomElement
+const getQuoteByPerson = (arr, personName) => {
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].person.toLowerCase() === personName.toLowerCase()) {
+      result.push(arr[i]);
+    }
+  }
+
+  return result;
+};
+
+module.exports = {
+  getRandomElement,
+  getQuoteByPerson,
 };
